b30e9b012ba6295e3ac8e104fe2aa81ab7d9944a
pnor <pnoreggio@gmail.com>
2023-07-17 13:35:20 -0400

    clauses

--- test/logic.cpp
+++ test/logic.cpp
@@ -39,8 +39,8 @@ TEST(PredicateTesting, isOdd) {
 }
 
 TEST(PredicateTesting, stringCompose) {
-  // Threre are 3 predicates in `stringCompose`. We test this function when all
-  // 3 predicates evaluate to both true or false in at least one test.
+  // Threre are 2 predicates in `stringCompose`. We test this function when all
+  // 2 predicates evaluate to both true or false in at least one test.
 
   // `(!dontSayOne && sayOne)` == true and `(sayTwo)` == true
   EXPECT_EQ(microtest::expectEquals<std::string>(
@@ -58,5 +58,57 @@ TEST(PredicateTesting, stringCompose) {
   // `(!dontSayOne && sayOne)` == false and `(sayTwo)` == false
   EXPECT_EQ(microtest::expectEquals<std::string>(
                 stringCompose(false, false, true), ""),
+            true);
+}
+
+TEST(ClauseTesting, isOdd) {
+  // For the simple function `isOdd`, there is only 1 clause:
+  // `x % 2 == 2`
+  // we can do expectations on the 2 possible values
+
+  EXPECT_EQ(microtest::expectEquals(isOdd(1), true), true);
+  EXPECT_EQ(microtest::expectEquals(isOdd(0), false), true);
+}
+
+TEST(ClauseTesting, stringCompose) {
+  // Threre are 3 predicates in `stringCompose`:
+  // - `sayOne`
+  // - `sayTwo`
+  // - `dontSayOne`
+
+  // `sayOne` == true, `sayTwo` == true, `dontSayOne` == true
+  EXPECT_EQ(microtest::expectEquals<std::string>(
+                stringCompose(true, true, true), "two"),
+            true);
+  // `sayOne` == true, `sayTwo` == true, `dontSayOne` == false
+  EXPECT_EQ(microtest::expectEquals<std::string>(
+                stringCompose(true, true, false), "one two"),
+            true);
+
+  // `sayOne` == true, `sayTwo` == false, `dontSayOne` == true
+  EXPECT_EQ(microtest::expectEquals<std::string>(
+                stringCompose(true, false, true), ""),
+            true);
+  // `sayOne` == true, `sayTwo` == false, `dontSayOne` == false
+  EXPECT_EQ(microtest::expectEquals<std::string>(
+                stringCompose(true, false, false), "one "),
+            true);
+
+  // `sayOne` == false, `sayTwo` == true, `dontSayOne` == true
+  EXPECT_EQ(microtest::expectEquals<std::string>(
+                stringCompose(false, true, true), "two"),
+            true);
+  // `sayOne` == false, `sayTwo` == true, `dontSayOne` == false
+  EXPECT_EQ(microtest::expectEquals<std::string>(
+                stringCompose(false, true, false), "two"),
+            true);
+
+  // `sayOne` == false, `sayTwo` == false, `dontSayOne` == true
+  EXPECT_EQ(microtest::expectEquals<std::string>(
+                stringCompose(false, false, true), ""),
+            true);
+  // `sayOne` == false, `sayTwo` == false, `dontSayOne` == false
+  EXPECT_EQ(microtest::expectEquals<std::string>(
+                stringCompose(false, false, false), ""),
             true);
 }
