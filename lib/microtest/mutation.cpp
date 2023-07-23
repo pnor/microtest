a3bf47e6ee843e1d123841955ec0d4a9ea4a73b5
pnor <pnoreggio@gmail.com>
2023-07-23 10:48:27 -0400

    mutations untested

--- lib/microtest/mutation.cpp
+++ lib/microtest/mutation.cpp
@@ -1,28 +1,85 @@
 #include "mutation.hpp"
 
-static void microtest::MutationTest::runMutationTests(
-    std::vector<std::function<bool()>> testFunctions,
-    std::vector<std::string> tags) {
-  // TODO
-  //
-  // create list of all tag combos (combinatorix)
-  // for each combo, run test suite
-  // record if it killed or survived
-  //
-  // return results
+// ===== Helper =====
+
+namespace microtest {
+static inline constexpr std::vector<std::vector<std::string>>
+stringCombinationsOfSize(const std::vector<std::string> &strings,
+                         int startIndex, int size) {
+  // TODO ?? this work
+  if (static_cast<int>(strings.size()) - startIndex - size < 0) {
+    return {};
+  } else if (size <= 0) {
+    return {{}};
+  }
+
+  std::vector<std::vector<std::string>> results;
+
+  for (int i = startIndex; i < strings.size() - size; i++) {
+    std::string cur = strings.at(i);
+    std::vector<std::vector<std::string>> rest =
+        stringCombinationsOfSize(strings, startIndex + 1, size);
+    for (auto &res : rest) {
+      res.push_back(cur);
+      results.push_back(res);
+    }
+  }
+
+  return results;
 }
 
-static void
-microtest::MutationTest::setAllTags(const std::vector<std::string> &tags) {
+// ===== Header =====
+
+/**
+ * Return all combinations of strings in `strings`
+ */
+static std::vector<std::vector<std::string>>
+stringCombinations(const std::vector<std::string> &strings) {
+  std::vector<std::vector<std::string>> results;
+  for (int i = 0; i < strings.size(); i++) {
+    std::vector<std::vector<std::string>> strs =
+        stringCombinationsOfSize(strings, 0, i);
+    results.insert(results.end(), strs.begin(), strs.end());
+  }
+  return results;
+}
+
+std::unordered_map<std::vector<std::vector<std::string>>, MutationTestResults>
+MutationTest::runMutationTests(std::vector<std::function<bool()>> testFunctions,
+                               std::vector<std::string> tags) {
+  // create list of all tag combos (combinatorix)
+  std::vector<std::vector<std::string>> tagCombinations =
+      stringCombinations(tags);
+
+  // for each combo, run test suite
+  // record if it killed or survived
+  std::unordered_map<std::vector<std::vector<std::string>>, MutationTestResults>
+      resultMap;
+
+  for (const std::vector<std::string> &tagCombo : tagCombinations) {
+    int killed = 0;
+    for (const auto &f : testFunctions) {
+      killed = f() ? killed : killed + 1;
+    }
+    MutationTestResults testResult(killed, testFunctions.size() - killed);
+    resultMap[tagCombo] = testResult;
+  }
+
+  return resultMap;
+}
+
+void MutationTest::setAllTags(const std::vector<std::string> &tags) {
   allTags.clear();
   for (const auto &t : tags) {
     allTags.insert(t);
   }
 }
 
-microtest::MutationTest::setActiveTags(const std::vector<std::string> &tags) {
+void MutationTest::setActiveTags(const std::vector<std::string> &tags) {
   activeTags.clear();
   for (const auto &t : tags) {
     activeTags.insert(t);
   }
 }
+
+}; // namespace microtest
--- lib/microtest/mutation.hpp
+++ lib/microtest/mutation.hpp
@@ -8,6 +8,18 @@
 
 namespace microtest {
 
+/** Class that reports the result of the test suite on a mutation. */
+struct MutationTestResults {
+public:
+  int testsKilled;
+  int testsSurvived;
+
+  MutationTestResults(int killed, int survived)
+      : testsKilled(killed), testsSurvived(survived){};
+
+  MutationTestResults() : testsKilled(0), testsSurvived(0){};
+};
+
 class MutationTest {
 public:
   MutationTest(MutationTest const &) = delete;
@@ -20,8 +32,10 @@ public:
    * Records the number of mutations that were "killed" by the test suite (at
    * least one test failed/returned false).
    */
-  static void runMutationTests(std::vector<std::function<bool()>> testFunctions,
-                               std::vector<std::string> tags);
+  static std::unordered_map<std::vector<std::vector<std::string>>,
+                            MutationTestResults>
+  runMutationTests(std::vector<std::function<bool()>> testFunctions,
+                   std::vector<std::string> tags);
 
   /** Returns the result of `lhs > rhs`.
    * If the tag is chosen to mutate, will instead return `lhs >= rhs`
@@ -30,9 +44,30 @@ public:
   static bool greaterThanMutatable(N lhs, N rhs, const std::string &tag) {
     MutationTest &mt = getInstance();
     if (mt.activeTags.contains(tag)) {
-      return lhs > rhs;
-    } else {
       return lhs >= rhs;
+    } else {
+      return lhs > rhs;
+    }
+  }
+
+  /** Returns the result of `lhs < rhs`.
+   * If the tag is chosen to mutate, will instead return `lhs <= rhs`
+   */
+  template <typename N>
+  static bool lessThanMutable(N lhs, N rhs, const std::string &tag) {
+    MutationTest &mt = getInstance();
+    if (mt.activeTags.contains(tag)) {
+      return lhs <= rhs;
+    } else {
+      return lhs < rhs;
+    }
+  }
+
+  /** When this mutation is active, increments `number` by 1. */
+  static void incrementMutable(int &number, const std::string &tag) {
+    MutationTest &mt = getInstance();
+    if (mt.activeTags.contains(tag)) {
+      number += 1;
     }
   }
 
